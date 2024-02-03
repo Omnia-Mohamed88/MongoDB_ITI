d6a0e5baf0a3bb21b350535ae68660943dee3d0f
Omnia-Mohamed88 <147095217+Omnia-Mohamed88@users.noreply.github.com>
2024-02-03 18:45:52 +0200

    Add files via upload

---
+++ lab2/lab2.1mongo.js
@@ -0,0 +1,94 @@
+//Qustion1
+// db.products.aggregate([
+// { $group:{_id: "$category",
+//     NumOfProfucts: {
+//         $sum:1
+//     }
+//     }
+//     }
+// ])
+
+
+//Question2
+// db.products.aggregate(
+// [
+// {$group:{_id: "$category",
+//     maxproductprice : {
+//         $max:"$price"}
+//         }},
+//         {$match:{_id: {$ne:null}}}])
+
+
+
+//AHmed orders and products
+// 
+// db.users.aggregate([
+//   {
+//     $match: {
+//       name: {
+//         $eq: "ahmed"
+//       }
+//     }
+//   },
+//   {
+//     $lookup: {
+//       from: "orders",
+//       localField: "_id",
+//       foreignField: "userId",
+//       as: "orders"
+//     }
+//   },
+//   {
+//     $lookup: {
+//       from: "products",
+//       localField: "orders.productsIds",
+//       foreignField: "_id",
+//       as: "orderProducts"
+//     }
+//   }
+//   
+// ])
+  
+  
+  
+  //highst order price 
+db.users.aggregate([
+  {
+    $match: {
+      name: "ahmed"
+    }
+  },
+  {
+    $lookup: {
+      from: "orders",
+      localField: "_id",
+      foreignField: "userId",
+      as: "orders"
+    }
+  },
+  {
+    $unwind: "$orders"
+  },
+  {
+    $lookup: {
+      from: "products",
+      localField: "orders.productsIds",
+      foreignField: "_id",
+      as: "orderProducts"
+    }
+  },
+  {
+    $unwind: "$orderProducts"
+  },
+  {
+    $group: {
+      _id: "$_id", 
+      highestOrderPrice: { $max: "$orderProducts.price" }
+    }
+  }
+])
+
+
+
+
+
---
+++ lab2/mongolab2.2.js
@@ -0,0 +1,69 @@
+//Using Match and groupby get the java categories books and group them to get total books and averagepagecount
+// 
+// db.books.aggregate([
+//   { $match: { categories: "Java" } },
+//   { $group: { _id: "$categories", totalBooks: { $sum: 1 }, averagePageCount: { $avg: "$pageCount" } } }
+// ]);
+// 
+
+
+
+
+
+//using $project and $adddFields and addtostring
+//1 include value fields 
+// db.books.aggregate([
+//   { 
+//     $project: {
+//       title: 1,
+//       pageCount: 1,
+//       publishedDate: 1,
+//       formattedDate: { $dateToString: { format: "%Y-%m-%d", date: "$publishedDate" } }
+//     }
+//   }
+// ]);
+
+
+
+// unwind of the authers array 
+// db.books.aggregate([
+//   { $unwind: "$authors" },
+//   { $group: { _id: "$authors", totalBooks: { $sum: 1 } } }
+// ]);
+
+
+//sort by count for the categories
+// db.books.aggregate([
+//   { $unwind: "$categories" },
+//   { $sortByCount: "$categories" }
+// ]);
+// 
+
+
+
+//using skip ad limit
+// let PageSize = 2;
+// let PageNumber = 2;
+// 
+// db.books.aggregate([
+//   { $skip: (PageNumber - 1) * PageSize },  // Skip the first (2 - 1) * 2 = 2 documents
+//   { $limit: PageSize },  // Take the next 2 documents
+//   { $count: "totalBooks" }  // Count the number of documents in this subset
+// ]);
+// 
+
+db.books.aggregate([
+  {
+    $project: {
+      title: 1,
+      firstAuthor: { $arrayElemAt: ["$authors", 0] },  
+      formattedDate: {
+        $dateToString: { format: "%Y-%m-%d", date: "$publishedDate" }
+      },
+      categoryAndTitle: { $concat: ["$title", " - ", { $arrayElemAt: ["$categories", 0] }] }  
+    }
+  }
+]);
+
+
+
